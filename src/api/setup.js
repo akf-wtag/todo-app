e66f00a4414d8311b4ec2b71281fc2cb940caa66
akf-wtag <faysal.akash@welldev.io>
2021-06-27 13:19:37 +0600

    back to previous setup

--- src/api/setup.js
+++ src/api/setup.js
@@ -1,6 +1,5 @@
 const axios = require('axios').create({
-  baseURL:
-    `${process.env.BASE_URL}:${process.env.PORT}` || 'http://localhost:3001',
+  baseURL: 'http://localhost:3001',
   headers: { 'Access-Control-Allow-Origin': '*' }, //bypasses CORS error
 });
 
